956e300085ad373dcf771fcfc9e6adf624877fc0
xuelan01.huang <xuelan01.huang@vipshop.com>
2018-04-17 16:30:29 +0800

    图片压缩、路径处理

---
+++ app/config/imgConfig.js
@@ -0,0 +1,6 @@
+var images = {
+    i01 : require("img/icon01.png"),
+    i02 : require("img/icon02.png")
+}
+
+module.exports =  images
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,51 +1,60 @@
 const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-// const ImageminPlugin = require('imagemin-webpack-plugin')
+const ImageminPlugin = require("imagemin-webpack-plugin").default
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 var entryConfig = require('./getConfig').entryConfig()
 var htmlOptions = require('./getConfig').htmlOptions
+var resolveConfig = require('./getConfig').resolveConfig
 
 
 module.exports = {
-    entry: entryConfig,           //唯一入口文件
+    entry: entryConfig,           //入口文件
     output: {
         path: __dirname + "/build",
         filename: "js/[name].js"
     },
-    devtool: 'eval-source-map',
-    devServer: {
-        contentBase: "./build",     //本地服务器所加载的页面所在的目录
-        historyApiFallback: true,   //不跳转
-        inline: true,
-        hot: true
-    },
     module: {
-        rules: [{
-            test: /(\.jsx|\.js)$/,
-            use: {
-                loader: "babel-loader"
+        rules: [
+            {
+                test: /(\.jsx|\.js)$/,
+                use: {
+                    loader: "babel-loader"
+                },
+                exclude: /node_modules/
             },
-            exclude: /node_modules/
-        },
-        //sass处理
-        {
-            test: /(\.css|\.scss)$/,
-            loader: ExtractTextPlugin.extract({
-                fallback: 'style-loader',
-                use: [{
-                    loader:'css-loader',
+            //sass处理
+            {
+                test: /(\.css|\.scss)$/,
+                loader: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [{
+                        loader:'css-loader',
+                        options: {
+                            minimize: true              //css压缩
+                        }
+                    },{
+                        loader:'sass-loader'
+                    }]
+                }),
+                exclude: /node_modules/
+            },
+            //图片压缩
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                use: {
+                    loader: 'url-loader',
                     options: {
-                        minimize: true              //css压缩
+                        limit: 10000
                     }
-                },{
-                    loader:'sass-loader'
-                }]
-            }),
-            exclude: /node_modules/
-        }]
+                },
+                exclude: /node_modules/
+            }
+        ]
     },
     plugins: [
+        new webpack.BannerPlugin('版权所有，翻版必究'),
         new CleanWebpackPlugin(
             ["build"],                  //匹配删除文件
             {
@@ -54,20 +63,19 @@ module.exports = {
                 dry: false        　　　　　　　　　　//启用删除文件
             }
         ),
-        new webpack.BannerPlugin('版权所有，翻版必究'),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
+        new webpack.optimize.CommonsChunkPlugin({ name: 'venders', filename: 'js/venders.js' }),          //合并公共文件
+        new webpack.DefinePlugin({
+            'process.env.NODE_ENV': '"production"'
+        }),
+        new webpack.optimize.OccurrenceOrderPlugin(true),           //排序输出，减小文件大小
+        new webpack.optimize.UglifyJsPlugin({                       //js压缩
             beautify: false,                     //紧凑输出
             comments: false,                    //删除所有注释
             compress: {
                 warnings: false                 //删除没用到的代码不输出警告
             }
         }),
-        new ExtractTextPlugin("css/[name].css"),        //合并css
-        // new ImageminPlugin({
-        //     pngquant: {
-        //         quality: '95-100'
-        //     }
-        // })
-    ].concat(htmlOptions)
+        new ExtractTextPlugin("css/[name].css")                //分离css
+    ].concat(htmlOptions),
+    resolve: resolveConfig
 };
